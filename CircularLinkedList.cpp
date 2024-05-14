033267b59709478a8d5b5f6f1a97ae9b49eb8f9a
Naimul Islam <naimulislam747@gmail.com>
2024-05-14 08:44:29 +0600

    added CircularDLL to it

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -50,7 +50,7 @@ struct CircularLinkedlist{
                 ptr = ptr->next;
             } 
             behind->next = ptr->next;
-            if(ptr == this->tail) this->tail = behind;
+            // if(ptr == this->tail) this->tail = behind;
             delete ptr;
         }
         
@@ -66,6 +66,95 @@ struct CircularLinkedlist{
         } while (current != this->head);
     }
 };
+
+
+struct CircularDLinkedlist{
+    struct Node{
+        bool value;
+        Node* next;
+        Node* prev;
+    };
+    Node* head;
+    Node* tail;
+    CircularDLinkedlist(){
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+    void printList(){
+        Node* ptr = this->head;
+        do{
+            cout << ptr->value << " ";
+            ptr = ptr->next;
+        }while(ptr != this->head);
+        
+    }    
+ void insertAtTail(bool x){
+        Node* newNode = new Node();
+        newNode->value = x;
+        if(!head){
+            newNode->next = newNode;
+            newNode->prev = newNode;
+            this->head = newNode;
+            this->tail = newNode;
+        } else if(this->head == this->tail){
+            newNode->next = this->head;
+            newNode->prev = this->head;
+            this->tail = newNode;
+            this->head->next = newNode;
+            this->head->prev = newNode;
+        }
+        else{
+            newNode->prev = this->tail;
+            newNode->next = this->head;
+            this->tail->next = newNode; 
+            this->head->prev = newNode;
+            this->tail = newNode;
+        }
+    }
+
+    bool play(){
+        bool aliceWon = false;
+        if(this->head == this->tail) return this->head->value;
+        while (this->head->next != this->tail ){
+            Node* ptr = this->head;
+            Node* temp = this->tail;
+            while(!(ptr->value)){
+                if(temp == ptr) return aliceWon;
+                ptr = ptr->next;
+            } 
+            ptr->next->value = !(ptr->next->value);
+            ptr->prev->value = !(ptr->prev->value);
+            if(ptr==this->head) this->head = this->head->next;
+            if(ptr==this->tail) this->tail = this->tail->prev;
+            ptr->prev->next = ptr->next;
+            ptr->next->prev = ptr->prev;
+            delete ptr; 
+            aliceWon = !aliceWon;
+        }
+        if(((this->head->value) && !(this->head->next->value)) || (!(this->head->value) && (this->head->next->value))) return !aliceWon;
+        return aliceWon;
+    }
+
+    void print(){
+        Node* ptr = this->head;
+        do {
+            cout << ptr->value << " ";
+            ptr = ptr->next;
+        } while (ptr != this->head);
+    }
+
+    ~CircularDLinkedlist(){
+        Node* current = this->head;
+        Node* next;
+        do{
+            next = current->next;
+            delete current;
+            current = next;
+        } while (current != this->head);
+    }
+};
+
+
 
 int main(){
     CircularLinkedlist* cycle1 = new CircularLinkedlist();
